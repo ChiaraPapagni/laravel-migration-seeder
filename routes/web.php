919d691b3d599b705019aa6e3db150d0be5a8268
Chiara <chiaraiors@gmail.com>
2022-01-17 14:10:37 +0100

    Add route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('flights', 'FlightController@index')->name('flights');
+
+Route::get('flights/{flight}', 'flightController@show')->name('flight');
