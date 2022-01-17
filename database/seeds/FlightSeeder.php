80d2613dff94132940c8b87979f56ac337e818fd
Chiara <chiaraiors@gmail.com>
2022-01-17 14:45:53 +0100

    Add seeder

--- database/seeds/FlightSeeder.php
+++ database/seeds/FlightSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Faker\Generator as Faker;
+use App\Models\Flight;
 use Illuminate\Database\Seeder;
 
 class FlightSeeder extends Seeder
@@ -9,8 +11,17 @@ class FlightSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+            $_flight = new Flight();
+            $_flight->airline = $faker->company();
+            $_flight->departure_airport = $faker->streetName();
+            $_flight->arrival_airport = $faker->streetName();
+            $_flight->destination = $faker->city();
+            $_flight->flight_duration = $faker->randomNumber(3, true);
+            $_flight->price = $faker->randomFloat(2);
+            $_flight->save();
+        }
     }
 }
